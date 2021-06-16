b47110eff325704874f55edc136a17ce42f9c548
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-16 02:45:49 +0100

    Initializing Mongoose connection

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv =  require("dotenv");
+const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
 const app = express();
 
 dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Connected to MongoDb...');
+    }
+);
 
 app.listen(8800, () => {
     console.log("Server is running!");
