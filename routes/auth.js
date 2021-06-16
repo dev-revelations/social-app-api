68ef7ddeb4791b1bb53fd8dee1cfaf54400df0ce
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-16 04:28:59 +0100

    Initial implementation of user register api

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get('/', (req, res) => {
-    res.send('Auth Home');
+router.post('/register', async (req, res) => {
+    const userObject = req.body;
+    const newUser = new User({
+        username: userObject.username,
+        email: userObject.email,
+        password: userObject.password
+    });
+
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
