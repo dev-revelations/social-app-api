f362f95fef299ba63ab4df470ac5740e0f83b537
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-16 04:41:48 +0100

    Protecting the user password with a hashed password using bcrypt package

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,25 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
 const User = require("../models/User");
 
+// REGISTER
 router.post('/register', async (req, res) => {
     const userObject = req.body;
-    const newUser = new User({
-        username: userObject.username,
-        email: userObject.email,
-        password: userObject.password
-    });
 
     try {
+        // Creating a new hashed password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(userObject.password, salt);
+
+        // Creating a new user
+        const newUser = new User({
+            username: userObject.username,
+            email: userObject.email,
+            password: hashedPassword
+        });
+
+        // Save user and return response
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (err) {
